2c45982a9617a2f06d2165d0edeb2dc54e00599d
trimbandaru <trimurthulu.bandaru@gmail.com>
2022-10-16 03:48:28 +0000

    new changes

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,8 @@
+/* global QUnit */
+
+sap.ui.require([
+	"com/demo/demofiori/test/integration/AllJourneys"
+], function() {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/integration/pages/Common.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/test/Opa5"
+], function(Opa5) {
+	"use strict";
+
+	return Opa5.extend("com.demo.demofiori.test.integration.pages.Common", {
+
+
+
+	});
+
+});
